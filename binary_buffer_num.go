0a2ea4417c56e2e153317bc6a9f8def9f0e625f5
Ivar Johansson <ivar@webbmaffian.se>
2024-02-04 14:37:34 +0100

    Binary buffer reader

--- binary_buffer_num.go
+++ binary_buffer_num.go
@@ -16,68 +16,34 @@ func (b *BinaryBuffer) WriteInt8(v int8) {
 	b.WriteByte(uint8(v))
 }
 
-// Write uint16 (little endian)
+// Write uint16
 func (b *BinaryBuffer) WriteUint16(v uint16) {
-	b.buf = append(b.buf,
-		byte(v),
-		byte(v>>8),
-	)
+	b.buf = binary.LittleEndian.AppendUint16(b.buf, v)
 }
 
-// Write int16 (little endian)
+// Write int16
 func (b *BinaryBuffer) WriteInt16(v int16) {
-	b.buf = append(b.buf,
-		byte(v),
-		byte(v>>8),
-	)
+	b.WriteUint16(uint16(v))
 }
 
 // Write uint32
 func (b *BinaryBuffer) WriteUint32(v uint32) {
-	b.buf = append(b.buf,
-		byte(v),
-		byte(v>>8),
-		byte(v>>16),
-		byte(v>>24),
-	)
+	b.buf = binary.LittleEndian.AppendUint32(b.buf, v)
 }
 
 // Write int32
 func (b *BinaryBuffer) WriteInt32(v int32) {
-	b.buf = append(b.buf,
-		byte(v),
-		byte(v>>8),
-		byte(v>>16),
-		byte(v>>24),
-	)
+	b.WriteUint32(uint32(v))
 }
 
 // Write uint64
 func (b *BinaryBuffer) WriteUint64(v uint64) {
-	b.buf = append(b.buf,
-		byte(v),
-		byte(v>>8),
-		byte(v>>16),
-		byte(v>>24),
-		byte(v>>32),
-		byte(v>>40),
-		byte(v>>48),
-		byte(v>>56),
-	)
+	b.buf = binary.LittleEndian.AppendUint64(b.buf, v)
 }
 
 // Write int64
 func (b *BinaryBuffer) WriteInt64(v int64) {
-	b.buf = append(b.buf,
-		byte(v),
-		byte(v>>8),
-		byte(v>>16),
-		byte(v>>24),
-		byte(v>>32),
-		byte(v>>40),
-		byte(v>>48),
-		byte(v>>56),
-	)
+	b.WriteUint64(uint64(v))
 }
 
 // Write int
