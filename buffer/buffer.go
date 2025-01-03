9c612c9e2804f50938405e166ec8c2f491773dd7
Ivar Johansson <ivar@webbmaffian.se>
2025-01-03 18:03:32 +0100

    Clean-up

--- buffer/buffer.go
+++ buffer/buffer.go
@@ -14,14 +14,11 @@ var (
 	_ io.ReaderFrom   = (*Buffer)(nil)
 	_ io.WriterTo     = (*Buffer)(nil)
 	_ fmt.Stringer    = (*Buffer)(nil)
-	_ io.ByteReader   = (*Buffer)(nil)
 )
 
 // A byte buffer, highly optimized to minimize allocations and GC pressure.
 type Buffer struct {
-	B   []byte
-	pos int
-	r   io.Reader
+	B []byte
 }
 
 func NewBuffer(size int) *Buffer {
@@ -40,12 +37,6 @@ func (b *Buffer) Cap() int {
 
 func (b *Buffer) Reset() {
 	b.B = b.B[:0]
-	b.ResetReader(nil)
-}
-
-func (b *Buffer) ResetReader(r io.Reader) {
-	b.pos = 0
-	b.r = r
 }
 
 func (b *Buffer) Bytes() []byte {
@@ -141,125 +132,4 @@ func (b *Buffer) Grow(n int) error {
 	}
 
 	return nil
-}
-
-// Release resets the reader state after processing a message.
-// It discards consumed data and prepares for the next message.
-func (b *Buffer) Release() {
-	b.release(0)
-}
-
-func (b *Buffer) ReleaseBefore(n int) {
-	b.release(b.croppedPos(n))
-}
-
-func (b *Buffer) ReleaseAfter(n int) {
-	b.pos = b.croppedPos(n)
-	b.B = b.B[:b.pos]
-}
-
-func (b *Buffer) croppedPos(n int) int {
-	return max(0, min(n, len(b.B)))
-}
-
-//go:inline
-func (b *Buffer) release(n int) {
-	b.B = append(b.B[n:n], b.B[b.pos:]...)
-	b.pos = n
-}
-
-func (b *Buffer) Pos() int {
-	return b.pos
-}
-
-func (b *Buffer) SetPos(n int) {
-	b.pos = b.croppedPos(n)
-}
-
-func (b *Buffer) AdjustPos(delta int) {
-	b.pos = b.croppedPos(b.pos + delta)
-}
-
-func (b *Buffer) Fill(n int) error {
-	required := n - (len(b.B) - b.pos)
-
-	if required <= 0 {
-		return nil
-	}
-
-	return b.fillFromReader(required)
-}
-
-// fill ensures that the buffer contains at least `n` bytes from the current position.
-func (b *Buffer) fillFromReader(required int) (err error) {
-	if b.r == nil {
-		return io.EOF
-	}
-
-	if err = b.Grow(required); err != nil {
-		return
-	}
-
-	size := len(b.B)
-	b.B = b.B[:cap(b.B)]
-
-	// Read data into the buffer.
-	nRead, err := io.ReadAtLeast(b.r, b.B[size:], required)
-	b.B = b.B[:size+nRead]
-
-	return
-}
-
-// consume advances the read position by `n` bytes.
-func (b *Buffer) consume(n int) {
-	b.pos += n
-}
-
-// ReadByte implements io.ByteReader.
-func (b *Buffer) ReadByte() (c byte, err error) {
-	c, err = b.PeekByte()
-
-	if err == nil {
-		b.consume(1)
-	}
-
-	return
-}
-
-// ReadByte implements io.ByteReader.
-func (b *Buffer) PeekByte() (c byte, err error) {
-	if err = b.Fill(1); err != nil {
-		return
-	}
-
-	return b.B[b.pos], nil
-}
-
-func (b *Buffer) ReadBytes(n int) (bytes []byte, err error) {
-	bytes, err = b.PeekBytes(n)
-
-	if err == nil {
-		b.consume(n)
-	}
-
-	return
-}
-
-func (b *Buffer) PeekBytes(n int) (bytes []byte, err error) {
-	if err = b.Fill(n); err != nil {
-		return
-	}
-
-	return b.B[b.pos : b.pos+n], nil
-}
-
-func (b *Buffer) ReadString(n int) (str string, err error) {
-	bytes, err := b.PeekBytes(n)
-
-	if err == nil {
-		str = fast.BytesToString(bytes)
-		b.consume(n)
-	}
-
-	return
 }
