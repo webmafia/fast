85665df0486bf0e893c5b13a606a2f9a89e8be27
Ivar Johansson <ivar@webbmaffian.se>
2025-02-11 13:31:06 +0100

    Clean-up

--- buffer/utils.go
+++ buffer/utils.go
@@ -1,6 +1,10 @@
 package buffer
 
-import "math/bits"
+import (
+	"math/bits"
+
+	"github.com/webmafia/fast/types"
+)
 
 func index(n int) int {
 	n--
@@ -37,4 +41,20 @@ func roundPow(n int) int {
 
 	// Add one to get the next power of 2
 	return n + 1
+}
+
+func sort[T types.Unsigned](a, b []T) {
+	for i := 1; i < len(a); i++ {
+		j := i - 1
+
+		// Move elements of `a[0...i-1]` that are smaller than `current` (descending order)
+		for j >= 0 && a[j] < a[i] {
+			a[j+1] = a[j]
+			b[j+1] = b[j]
+			j--
+		}
+		// Place the current element at its correct position
+		a[j+1] = a[i]
+		b[j+1] = b[i]
+	}
 }
